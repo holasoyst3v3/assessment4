b5ed5a6e9a0e161938d02e0a2c0beb1dd23b936c
holasoyst3v3 <s.ljr.estrada@gmail.com>
2021-11-19 10:15:37 -0600

    initial commit

---
+++ assessment-backend/server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+app.get("/api/fortune", (req, res) => {
+  const fortune = ["Sometimes, spilling ketchup on your favorite shirt means no more nice shirt today.",
+					          "When it is cold out, wear something warm",
+					          "One day you will look back on your life and think, 'Maybe I should have order the chicken.' Order the chicken.",
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortune.length);
+  let randomFortune = fortune[randomIndex];
+
+  res.status(200).send(randomFortune);
+  
+});
+
+app.listen(4000, () => console.log("COMPLIMENTS initializing...... running on 4000"));
+
+app.listen(4001, () => console.log("FORTUNES initializing...... running on 4001"));
