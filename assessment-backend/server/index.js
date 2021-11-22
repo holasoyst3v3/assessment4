9ae881782f5864f7ac887974df4e2e9a1367dd2d
holasoyst3v3 <s.ljr.estrada@gmail.com>
2021-11-21 23:44:48 -0600

    semi final commit

--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -47,9 +47,79 @@ app.get("/api/fortune", (req, res) => {
   
 });
 
+// const input = [];
+
+// app.post('api/input;', (req,res) => {
+//   input.push(req.body.newInput)
+
+//   input.reverse();
+//   res.status(200).send(input)
+
+// });
+
+                  // WALL PAGE CODE//
+//**************************************************//
+// Document not defined ERROR with code below //
+
+// document.querySelector("form").addEventListener("submit",addQuote);
+// const message = document.querySelector('#message')
+
+function addQuote(event){
+    event.preventDefault();
+    let inputField = document.querySelector('input')
+    
+    const quote = document.createElement('li')
+    
+    const listQuote = document.createElement('span');
+    listQuote.textContent = inputField.value;
+    listQuote.addEventListener("click", crossOffQuote);
+    quote.appendChild(listQuote);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.addEventListener("click", deleteQuote);
+    quote.appendChild(deleteBtn);
+
+
+    const list = document.querySelector("ul");
+    list.appendChild(quote);
+
+    message.textContent = `New quote added to the Wall.`
+    revealMessage()
+
+    inputField.value = ""
+}
+
+function deleteQuote(event){
+    revealMessage()
+    event.target.parentNode.remove();
+}
+
+function crossOffQuote(event){
+    event.target.classList.toggle("checked")
+    // if (event.target.classList.contains("checked") === true) {
+    //     message.textContent = `${event.target.textContent} watched!`
+    // } else {
+    //     message.textContent = `${event.target.textContent} added yet again...`
+    // }
+    revealMessage()
+}
+function revealMessage() {
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
+}
+
+//WALL PAGE CODE //
+
+
+
 
 app.listen(4000, () => console.log("COMPLIMENTS initializing...... running on 4000"));
 
 app.listen(4001, () => console.log("NOTCOMPLIMENT initializing...... running on 4001"));
 
 app.listen(4002, () => console.log("FORTUNES initializing...... running on 4002"));
+
+app.listen(4003, () => console.log("INPUTS initializing...... running on 4003"));
